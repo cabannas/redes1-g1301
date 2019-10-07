6041584a8e9a5ad3b93a615efd0f8c065b33b6fe
Daniel Cabanas <daniel.cabannas@estudiante.uam.es>
2019-10-07 15:24:53 +0200

    cambio en close

--- materialP1/practica1.py
+++ materialP1/practica1.py
@@ -31,14 +31,14 @@ def signal_handler(nsignal,frame):
 	logging.info('Control C pulsado')
 	if handle:
 		pcap_breakloop(handle)
-		
+
 
 def procesa_paquete(us,header,data):
 	global num_paquete, pdumper
-	
+
 	logging.info('Nuevo paquete de {} bytes capturado a las {}.{}'.format(header.len,header.ts.tv_sec,header.ts.tv_usec))
 	num_paquete += 1
-	
+
 	#TODO imprimir los N primeros bytes
 	N = args.nbytes
 
@@ -55,12 +55,12 @@ def procesa_paquete(us,header,data):
 		print('Los %d bytes del paquete no.%d:' % (N, num_paquete))
 
 	d = data[0: N]
-	
+
 	#NOTA: calculamos el numero de lineas que se van a necesitar (cada linea va a ocupar como mucho 16 bytes)
 	lineas = int(len(d)/16) + 1
 
 	for i in range(0, lineas):
-		#NOTA: coge los bytes de 16 en 16 
+		#NOTA: coge los bytes de 16 en 16
 		l = d[16*i: 16*(i+1)]
 
 		#NOTA: bytes del paquete capturado expresados en hexadecimal, con 2 digitos por Byte (separados por espacios en blanco)
@@ -71,7 +71,7 @@ def procesa_paquete(us,header,data):
 
 	#NOTA: modificamos la fecha del paquete capturado, sumandole 30 min (30*60 segundos)
 	header.ts.tv_sec += TIME_OFFSET
-	
+
 	#Escribir el tráfico al fichero de captura con el offset temporal
 	if args.interface:
 		pcap_dump(pdumper, header, data)
@@ -105,21 +105,21 @@ if __name__ == "__main__":
 	pdumper = None
 	descr2 = None
 
-	
+
 	#TODO abrir la interfaz especificada para captura o la traza
-	
+
 	if args.interface:
 		handle = pcap_open_live(args.interface, ETH_FRAME_MAX, NO_PROMISC, TO_MS, errbuf)
 		if handle is None:
 			logging.error('Error al abrir interfaz')
 
 
-	elif args.tracefile: 
+	elif args.tracefile:
 		handle = pcap_open_offline(args.tracefile, errbuf)
 		if handle is None:
 			logging.error('Error al abrir traza')
 
-	
+
 
 	#TODO abrir un dumper para volcar el tráfico (si se ha especificado interfaz)
 
@@ -129,7 +129,7 @@ if __name__ == "__main__":
 			logging.error('Error al abrir descriptor de archivo pcap')
 
 		fname = 'captura.' + args.interface + '.' + str(int(time.time())) + '.pcap'
-		
+
 		pdumper = pcap_dump_open(descr2, fname)
 		if pdumper is None:
 			logging.error('Error al crear objeto dumper para guardar los paquetes')
@@ -137,10 +137,10 @@ if __name__ == "__main__":
 
 
 	ts = time.time()
-	print('Tiempo actual UNIX en segundos: ' + str(ts))	
+	print('Tiempo actual UNIX en segundos: ' + str(ts))
 	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 	print('Fecha actual: ' + str(st) + '\n\n')
-	
+
 
 	ret = pcap_loop(handle,50,procesa_paquete,None)
 	if ret == -1:
@@ -150,18 +150,20 @@ if __name__ == "__main__":
 	elif ret == 0:
 		logging.debug('No mas paquetes o limite superado')
 	logging.info('{} paquetes procesados'.format(num_paquete))
-	
+
 
 	#TODO si se ha creado un dumper cerrarlo
 
-	if args.interface:
-		pcap_dump_close(pdumper)
-		pcap_close(descr2)
-		pcap_close(handle)
-		
-	elif args.tracefile:
-		pcap_close(handle)
-		
+    pcap_close(handle)
 
-	
+    if args.interface:
+        pcap_dump_close(pdumper)
+    	pcap_close(descr2)
 
+	# if args.interface:
+	# 	pcap_dump_close(pdumper)
+	# 	pcap_close(descr2)
+	# 	pcap_close(handle)
+    #
+	# elif args.tracefile:
+	# 	pcap_close(handle)
